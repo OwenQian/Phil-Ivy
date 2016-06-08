fef79df6b25adf95f5620a2b5a719dba83427b27
OwenQian <oyqian@uwaterloo.ca>
2016-06-08 00:53:17 -0400

    adding comments to fast_eval

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 //#include <stdio.h>
 
 int main() {
-	int c1 = (0x1000);	//ace of spades
-	int c2 = (0x1000);	//king of diamonds
-	int c3 = (0x1000);	//3 of hearts
-	int c4 = (0x1000);	//7 of hearts
-	int c5 = (0x1001);	//10 of spades
+	int c1 = (0x00010000);	//ace of spades
+	int c2 = (0x10000000);	//king of diamonds
+	int c3 = (0x10000000);	//3 of hearts
+	int c4 = (0x10000000);	//7 of hearts
+	int c5 = (0x10010000);	//10 of spades
 
     int q = (c1 | c2 | c3 | c4 | c5) >> 16;
 	std::cout << q << std::endl;
