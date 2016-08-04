d1879f07590100e8fb900bfca22ca946289df197
yizhouuuuu <yizhoudeng@gmail.com>
2016-08-04 01:01:27 -0400

    added the playround function

--- GameUtilities/GameUtilities.cpp
+++ GameUtilities/GameUtilities.cpp
@@ -6,6 +6,8 @@
 
 #include "GameUtilities.h"
 #include "helper.h"
+#include "../Nodes/ChoiceNode.h"
+#include "../Nodes/OpponentNode.h"
 
 std::vector<int> deal(	std::vector<int>	&previousDeck,
 						int					state) {
@@ -44,10 +46,10 @@ void init_deck(std::vector<int>& deck) {
 }
 
 void playGame(){
-	double bigBlind = 50.0;
-	double smallBlind = 25.0;
+	static double bigBlind = 50.0;
+	static double smallBlind = 25.0;
 	double initialChips = 1000.0;
-	int smallBlindPosition = 0;
+	static int smallBlindPosition = 0;
 	
 	Player botPlayer(0, 0, initialChips, 0); //0, 0 represetn dummy cards
 	Player oppPlayer(0, 0, initialChips, 0);
@@ -56,5 +58,43 @@ void playGame(){
 		std::vector<Player> updatePlayers = playRound (botPlayer, oppPlayer);
 		botPlayer = updatePlayers[0];
 		oppPlayer = updatePlayers[1];
+		smallBlindPosition = ~smallBlindPosition;
 	}
+}
+
+std::vector<Player> playRound(Player botPlayer, Player oppPlayer){
+	enum states {HOLECARDS, PREFLOP, FLOP, TURN, RIVER};
+	std::vector<int> deck;
+	init_deck(deck);
+	botPlayer.setHoldCards(deal(deck, HOLECARDS));
+	oppPlayer.setHoleCards(deal(deck, HOLECARDS));
+	int currentStage = 0;
+	if (smallBlindPosition == 0){
+		auto root = std::make_shared<ChoiceNode>(0, bigBlind + smallBlind, std::vector<int>(),\
+		botPlayer, oppPlayer, smallBlindPosition, std::shared_ptr<ChoiceNode> (NULL));
+	} else if (smallBlindPosition == 1){
+		auto root = std::make_shared<OpponentNode>(0, bigBlind + smallBlind, std::vector<int>(),\
+		botPlayer, oppPlayer, smallBlindPosition, std::shared_ptr<OpponentNode> (NULL));
+	} else {
+		std::cout << "fuck owen, also why is smallblindposition not 1 or 0" << std::endl;
+		return std::vector<Player>();
+	}
+	//infers which type of node it is
+	auto currentNode = root;
+	while (currentNode != NULL) { //might ahve to be shared_ptr null
+		currentNode = playTurn(currentNode);
+		if (currentStage != currentNode.getGame().getState()){
+			currentStage++; //goes to the next stage in the game
+			std::vector<int> newBoard = deal(deck, currentStage);
+			//adding current board cards to newly dealt cards
+			for (auto i = currentNode.getGame().getBoardCards().begin();
+			i != currentNode.getGame().getBoardCards().end(); ++i){
+				newBoard.push_back(*i);
+			}
+			currentNode.getGame().setBoardCards(newBoard); //cards are reversed in order, might mess up when we display the cards
+		}
+	}
+	// currentNode.getBotPlayer or soemtihng like that
+	std::vector<Player> updatePlayers{botPlayer, oppPlayer};
+	return updatePlayers;
 }
