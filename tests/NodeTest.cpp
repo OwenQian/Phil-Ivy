fda46922b74fccd1653f0fe7f0ded097f490e77c
Max Deng <Max Deng>
2016-08-30 22:53:58 -0400

    created test for node

--- tests/NodeTest.cpp
+++ tests/NodeTest.cpp
@@ -1,20 +1,24 @@
-#include "Node.h"
+#include "../Nodes/ChoiceNode.h"
+#include "../Nodes/OpponentNode.h"
+#include "../Config.h"
+#include "../Player.h"
+#include "../GameObject.h"
 
 #include <vector>
 
 int main() {
-	static const double smallBlind = 5.0;
-	static const double bigBlind = 10.0;
 	int myState = 0;
 	double myPot = 10.0;
 	std::vector<int> v{1,2};
 	Player Max(13, 15, 1000, 0);
 	Player Owen(13, 15, 1000, 0);
-	int pTurn = 2;
-	Node n(myState,
+	int pTurn = 0;
+	ChoiceNode n(myState,
 			myPot,
 			v,
 			Max,
 			Owen,
-			pTurn);
+			pTurn,
+			nullptr);
+	n.call();
 }
