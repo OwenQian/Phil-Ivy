ee69bd9204d850aafb68d72282913eab64ff8ecf
yizhouuuuu <yizhoudeng@gmail.com>
2016-09-02 01:27:58 -0400

    Update the loop for player turn

---
+++ Config.cpp
@@ -0,0 +1,8 @@
+#include "Config.h"
+extern const int monteCarloDuration = 5;    // in seconds
+extern const double exploreConst = 10.0;
+extern const double smallBlind = 25.0;
+extern const double bigBlind = 50.0;
+extern const double initialChips = 1000.0;
+extern int smallBlindPosition = 0;
+
--- Config.h
+++ Config.h
@@ -1,11 +1,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-static const int monteCarloDuration = 5;    // in seconds
-static const double exploreConst = 10.0;
-static const double smallBlind = 25.0;
-static const double bigBlind = 50.0;
-static const double initialChips = 1000.0;
-static int smallBlindPosition = 0;
+extern const int monteCarloDuration;    // in seconds
+extern const double exploreConst;
+extern const double smallBlind;
+extern const double bigBlind;
+extern const double initialChips;
+extern int smallBlindPosition;
 
 #endif
