7be2453f4e5d9d82aa440ffa04f990104f553ab7
OwenQian <oyqian@uwaterloo.ca>
2016-09-22 21:26:35 -0400

    Removed unnecessary includes from main

--- main.cpp
+++ main.cpp
@@ -1,16 +1,4 @@
-#include "Player.h"
-#include "GameObject.h"
 #include "Nodes/Node.h"
-#include "potInfo.h"
-#include "handEval/helper.h"
-#include "GameUtilities/GameUtilities.h"
-#include "Nodes/ChoiceNode.h"
-#include "Nodes/OpponentNode.h"
-#include "Stage.h"
-
-#include <vector>
-#include <memory>
-#include <iostream>
 
 int main() {
     Node::playGame();
